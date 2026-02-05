a3c434caf361a820860c43a4ab6a0e48defc9dec
Rinchhen Thing <lamakiso588@gmail.com>
2026-02-05 12:42:23 +0545

    added the delete and update routes

--- book_api/routers/authors.py
+++ book_api/routers/authors.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends 
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session 
 from database import SessionLocal
 from models.author import Author
-from schemas.author import AuthorCreate, AuthorResponse 
+from schemas.author import AuthorCreate, AuthorResponse, AuthorUpdate
+
+#unq : routers/authors 
 
 router = APIRouter(
     prefix="/authors",
@@ -37,6 +39,36 @@ def get_author(author_id:int, db:Session = Depends(get_db)):
     if not author:
         raise HTTPException(status_code=404, details="Author not found")
     return author 
+
+#now to update the data
+@router.put("/{author_id}", response_model=AuthorResponse)
+def update_author(
+        author_id: int,
+        author_update: AuthorUpdate,
+        db: Session: Depends(get_db)
+):
+    #first fetch the record 
+    author = db.query(Author).filter(Author.id == author_id).first()
+    if not author:
+        raise HTTPException(status_code=404, details="Author not found")
+    
+    #then update the record 
+    if author_update.name is not None: 
+        author.name = author_update.name 
+
+    db.commit()
+    db.refresh(author)
+    return author 
+
+#and now to delete 
+@router.delete("/{author_id}")
+def delete_book(author_id: int, db: Session = Depends(get_db)):
+    author = db.query(Author).filter(Author.id == author_id).first()
+    if not author:
+        raise HTTPException(status_code=404, details="Author not found")
+    db.delete(Author)
+    db.commit()
+    return {"message": "Author deleted"}
 
 
 
--- book_api/routers/books.py
+++ book_api/routers/books.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends 
+from fastapi import APIRouter, Depends, HTTPException 
 from sqlalchemy.orm import Session 
 from database import SessionLocal
 from models.book import Book 
-from schemas.book import BookCreate, BookResponse 
+from schemas.book import BookCreate, BookResponse, BookUpdate
+
+#unq: routers/books
 
 router = APIRouter(
     prefix="/books",
@@ -41,3 +43,35 @@ def get_book(book_id: int, db:Session = Depends(get_db)):
         raise HTTPException(status=404, details="Book not found")
     return book 
 
+#to update the books field 
+@router.put("/{book_id}", response_model=BookResponse)
+def update_book(
+        book_id: int,
+        book_update: BookUpdate,
+        db: Session = Depends(get_db)
+):
+    #first fetcht the records
+    book = db.query(Book).filter(Book.id == book_id).first()
+    if not book:
+        raise HTTPException(status=400, details="Book not found")
+
+    #not lets update what is to from the input 
+    if book_update.title is not None:
+        book.title = book_update.title
+    if book_update.author_id is not None:
+        bool.author_id = book_update.author_id
+
+    db.commit()
+    db.refresh(book)
+    return book 
+
+#and now to delete 
+@router.delete("/{book_id}")
+def delete_book(book_id: int, db: Session = Depends(get_db)):
+    book = db.query(Book).filter(Book.id == book_id).first()
+    if not book:
+        raise HTTPException(status=400, details="Book not found")
+
+    db.delete(book)
+    db.commit()
+    return {"message": "Book Deleted"}
